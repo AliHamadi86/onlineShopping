d6052a66889f82a43d6a0aecb3ed1bc0e93bdecc
ali hamadi <ali.hamadi@smule.com>
2019-06-13 08:21:30 +0300

    NOJIRA: add search method to the item

--- src/main/java/com/onlineShopping/item/controller/ItemController.java
+++ src/main/java/com/onlineShopping/item/controller/ItemController.java
@@ -1,5 +1,7 @@
 package com.onlineShopping.item.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.onlineShopping.item.model.Item;
@@ -37,6 +40,16 @@ public class ItemController {
 	@DeleteMapping("/item/{id}")
 	void delete(@PathVariable Integer id) {
 		itemService.deleteById(id);
+	}
+
+	@GetMapping("/item/{id}")
+	Optional<Item> read(@PathVariable Integer id) {
+		return itemService.findById(id);
+	}
+
+	@GetMapping("/item/search")
+	Iterable<Item> findByQuery(@RequestParam("name") String name) {
+		return itemService.findByName(name);
 	}
 
 }
--- src/main/java/com/onlineShopping/item/service/ItemService.java
+++ src/main/java/com/onlineShopping/item/service/ItemService.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.onlineShopping.item.model.Item;
 
 public interface ItemService extends CrudRepository<Item, Integer> {
-
+	Iterable<Item> findByName(String name);
 }
